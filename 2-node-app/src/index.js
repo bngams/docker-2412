314acc7cbf22d76622111401f9fa74039fafc266
bngams <n.boris@devidia.net>
2024-11-27 10:01:07 +0000

    docker compose example

--- 2-node-app/src/index.js
+++ 2-node-app/src/index.js
@@ -9,9 +9,10 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World!!!');
 });
 
+// Start App on port
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
 }); 
